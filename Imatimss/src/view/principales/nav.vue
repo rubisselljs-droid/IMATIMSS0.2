adf1781f21b610ecbed205916a8495a860b67268
Josias Ek Tun <119262581+JosiasET@users.noreply.github.com>
2026-09-24 10:56:05 -0600

    Nav haciendo

---
+++ Imatimss/src/view/principales/nav.vue
@@ -0,0 +1,26 @@
+<script>
+  import {imss} from '../../assets/image/logoImss.png'
+</script>
+
+
+<template>
+
+    <nav class="grid grid-cols-layout gap-4 ">
+        
+            <div class="border-2 border-amber-300" >
+                <img :src="imss " alt="Logo">
+            </div>
+
+            <div 
+            class="grid grid-cols-layout gap-4 
+            
+            
+            border-2 border-amber-300">
+                <a href="" class="text-primary">Menu</a>
+                <a href="" class="text-primary">Consultas</a>
+                <a href="" class="text-primary">Maquinas</a>
+            </div>
+       
+    </nav>
+
+</template>
